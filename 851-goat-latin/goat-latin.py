10d05ef58ba6c2d5f3788c6146a507f50a2bb0f3
Chitundu Lucky <87910852+luckychitundu@users.noreply.github.com>
2024-09-16 22:01:20 -0500

    Time: 41 ms (17.11%) | Memory: 16.6 MB (27.19%) - LeetSync

--- 851-goat-latin/goat-latin.py
+++ 851-goat-latin/goat-latin.py
@@ -1,10 +1,9 @@
 class Solution:
-    def toGoatLatin(self, sentence: str) -> str:
-        vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"] 
+    def toGoatLatin(self, sentence: str) -> str: 
         words = sentence.split() 
         for i in range(len(words)):
             word = words[i]
-            if word[0] in vowels:
+            if word[0].casefold() in "aeiou":
                 word = word + "ma" + "a" * (i + 1)
                 words[i] = word 
             else:
