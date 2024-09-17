50bcf237fcbc3d45785db1d0be29764de6b29688
Chitundu Lucky <87910852+luckychitundu@users.noreply.github.com>
2024-09-17 16:19:58 -0500

    Time: 28 ms (96.55%) | Memory: 17.2 MB (13.04%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.py
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.py
@@ -10,30 +10,23 @@ class Node:
 class Solution:
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
-            return head
-        
-        # Use a stack to store the nodes we need to visit later (when we're done with the child)
-        stack = []
-        curr = head
-        
+            return head 
+        temp = []         
+        curr = head 
         while curr:
             if curr.child:
-                # If the current node has a child, we'll push the next node onto the stack (if it exists)
                 if curr.next:
-                    stack.append(curr.next)
-                    curr.next.prev = None  # Detach it for now
-                
-                # Then, we link the current node to its child
-                curr.next = curr.child
-                curr.child.prev = curr
-                curr.child = None  # Clear the child pointer after flattening
-            elif not curr.next and stack:
-                # If we're at the end of the current list but have stored nodes on the stack
-                next_node = stack.pop()
-                curr.next = next_node
-                next_node.prev = curr
-            
-            # Move to the next node
-            curr = curr.next
+                    temp.append(curr.next)
+                    curr.next.prev = None
+
+                curr.next = curr.child 
+                curr.child.prev = curr 
+                curr.child = None 
+            elif not curr.next and temp:
+                node = temp.pop() 
+                curr.next = node 
+                node.prev = curr     
+            curr = curr.next    
         
-        return head
+        return head          
+               
